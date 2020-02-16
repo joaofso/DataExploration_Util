e05fb34a2d83e81c23349423bdce7ef2d002d07d
João Felipe Silva Ouriques <joaofso@gmail.com>
2020-02-16 20:59:30 +0100

    First version of the plotter
    * Plotter just generates scatter plots
    * Still untested

--- csv_plot.py
+++ csv_plot.py
@@ -1,31 +1,38 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 
 from argparse import ArgumentParser
 import sys
 import os
-import pandas as pd
-import numpy as np
+import pandas
+import matplotlib.pyplot as plot
 from pandas.errors import ParserError
 
 
 def get_command_parser():
-    parser = ArgumentParser(description='Runs a simple scatter plot on the provided csv file.')
+    parser = ArgumentParser(description='Plots the desired graphs based on the information from the provided csv file.')
     # positional arguments
     parser.add_argument('file', help='location of the CSV file')
-    parser.add_argument('-x', '--x-axis', nargs='+', dest='x', default=[],
+
+    parser.add_argument('-x', '--x-axis', nargs='*', dest='x', default=[],
                         help='list of columns to be considered as x-axis')
-    parser.add_argument('-y', '--y-axis', nargs='+', dest='y', default=[],
+
+    parser.add_argument('-y', '--y-axis', nargs='*', dest='y', default=[],
                         help='list of columns to be considered as y-axis')
+
     parser.add_argument('-sep', '--separator', dest='separator', default=';',
                         help='separator used as field delimiter of the CSV files (\';\' is the default separator)')
+
     parser.add_argument('-l', '--list-columns', dest='list', default=False,
                         help='prints a list of columns available to be plotted')
+
+    parser.add_argument('-f', '--save_file', dest='out', help='saves the graphs in a file')
     return parser
 
 
 def check_parameters():
     parameter_parser = get_command_parser()
     args = parameter_parser.parse_args()
+
     if not args.file:
         raise Exception('File not present. Provide an input file.')
 
@@ -35,6 +42,14 @@ def check_parameters():
     if not is_csv(args.file):
         raise Exception('The provided file is not a csv.')
 
+    if not args.list:
+        if not args.x:
+            raise Exception('Provide a list of column to be considered as x-axis')
+        if args.y:
+            raise Exception('Provide a list of column to be considered as y-axis')
+
+    if args.out and not args.out.endswith('.png'):
+        args.out = args.out + '.png'
     return args
 
 
@@ -42,24 +57,35 @@ def is_csv(file_path):
     return file_path.endswith('.csv')
 
 
-def generate_plot(args):
+def generate_plots(args):
     try:
-        dataframe = pd.read_csv(args.file, sep=args.separator)
-        x_axis = args.columns if args.columns else dataframe.columns
-        sub_dataframe = dataframe[columns]
-        calculate_metrics(sub_dataframe)
+        dataframe = pandas.read_csv(args.file, sep=args.separator)
+        for field in set(args.x, args.y):
+            if field not in dataframe.columns:
+                raise Exception('The column {} is not present in the provided csv file'.format(field))
+
+        number_graphs = len(args.x) * len(args.y)
+        current_graph = 0
+        figure, graphs = plot.subplots(nrows=number_graphs)
+
+        for x in args.x:
+            for y in args.y:
+                x_sample = dataframe[x]
+                y_sample = dataframe[y]
+                graphs[current_graph].scatter(x=x_sample, y=y_sample)
+                graphs[current_graph].xlabel(x)
+                graphs[current_graph].ylabel(y)
+                current_graph += 1
+
+        plot.show()
+
     except ParserError as exc:
         print('The provided separator {} is incompatible with the provided file'.format(args.separator))
-
-def calculate_metrics(df):
-    summary = df.describe(include=[np.number])
-    print(summary)
-
 
 if __name__ == '__main__':
     try:
         arguments = check_parameters()
-        perform_summary(arguments)
+        generate_plots(arguments)
         sys.exit(0)
     except Exception as ex:
         print(ex)

4ff3f781413ee6593566719d63b4a94a1c9bcb71
Joao Felipe <joao.felipe.ouriques@ericsson.com>
2020-05-14 09:20:18 +0200

    Adding the option to run the summary script by piping the standard output

--- csv_summary.py
+++ csv_summary.py
@@ -11,8 +11,10 @@ from pandas.errors import ParserError
 def get_command_parser():
     parser = ArgumentParser(description='Runs a simple textual summary on the provided csv file.')
     # positional arguments
-    parser.add_argument('file', help='location of the CSV file')
+    parser.add_argument('file', type=str, nargs='?', help='location of the CSV file')
     parser.add_argument('-c', '--columns', nargs='+', default=[], help='list of columns to be summarized')
+    parser.add_argument('-l', '--list-columns', dest='list', action='store_true',
+                        help='prints a list of columns available to be summarized')
     parser.add_argument('-sep', '--separator', dest='separator', default=';',
                         help='separator used as field delimiter of the CSV files (\';\' is the default separator)')
     return parser
@@ -21,14 +23,12 @@ def get_command_parser():
 def check_parameters():
     parameter_parser = get_command_parser()
     args = parameter_parser.parse_args()
+
+    if not args.file and not sys.stdin.isatty():
+        args.file = sys.stdin
+
     if not args.file:
         raise Exception('File not present. Provide an input file.')
-
-    if not os.path.exists(args.file):
-        raise Exception('The provided file does not exist.')
-
-    if not is_csv(args.file):
-        raise Exception('The provided file is not a csv.')
 
     return args
 
@@ -39,12 +39,14 @@ def is_csv(file_path):
 
 def perform_summary(args):
     try:
-        dataframe = pd.read_csv(args.file, sep=args.separator)
-        columns = args.columns if args.columns else dataframe.columns
-        sub_dataframe = dataframe[columns]
-        calculate_metrics(sub_dataframe)
+        df = pd.read_csv(args.file, sep=args.separator, engine='python')
+        columns = args.columns if args.columns else df.columns
+        sub_df = df[columns]
+        calculate_metrics(sub_df)
     except ParserError as exc:
-        print('The provided separator {} is incompatible with the provided file'.format(args.separator))
+        print('The provided separator {} is incompatible with the provided file or the provided file is not a valid CSV'
+              .format(args.separator))
+
 
 def calculate_metrics(df):
     summary = df.describe(include=[np.number])
@@ -54,7 +56,11 @@ def calculate_metrics(df):
 if __name__ == '__main__':
     try:
         arguments = check_parameters()
-        perform_summary(arguments)
+        if arguments.list:
+            dataframe = pd.read_csv(arguments.file, sep=arguments.separator, engine='python')
+            print(dataframe.columns.tolist())
+        else:
+            perform_summary(arguments)
         sys.exit(0)
     except Exception as ex:
         print(ex)
